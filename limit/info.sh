8a5fbe6a93e99d15a04606edc26d7030c5c21d88
PENDY <valenrock86@gmail.com>
2024-07-10 17:09:10 +0700

    uploads

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "Quin Vpn Premium Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @Quin_Storeb" | lolcat
+echo "                   Author Script Quin Vpn Premium" | lolcat
+echo "-----------------------------------------------------"
---
+++ menu/menu1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sS "https://raw.githubusercontent.com/bheghenkz/vip/main/menu/dashboard.sh" | bash
